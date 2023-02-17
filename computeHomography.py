9099695ca7fad3a415bdf163dd02219aece4dbc9
Dharini Raghavan <dhariniraghavan2001@gmail.com>
2023-02-17 13:36:23 +0530

    Update computeHomography.py

--- computeHomography.py
+++ computeHomography.py
@@ -1,6 +1,7 @@
-#pts_src[] = 
-#pts_dst[] = 
+pts_src[] = # object points 
+pts_dst[] = # image points
 #finding homography
+import cv2
 h, status = cv2.findHomography(pts_src, pts_dst)
 #warping source image to destination
 im_dst = cv2.warpPerspective(im_src, h, size)
