7c35a9af47688ad07525362277390e1694468785
Shashank Prakash <shashankprakash1809@gmail.com>
2023-02-20 23:57:03 +0530

    first commit

--- computeHomography.py
+++ computeHomography.py
@@ -5,17 +5,22 @@ pts_dst=np.array([[663,451,1],[755,449,1],[740,388,1],[655,390,1],[636,274,1],[7
 import cv2
 h, status = cv2.findHomography(pts_src, pts_dst)
 print(h)
-im_src=cv2.imread('/home/rbc2080ti/Desktop/OpenCV/saved_img.jpg')
+#im_src=cv2.imread('/home/rbc2080ti/Desktop/OpenCV/saved_img.jpg')
 # warping source image to destination
-im_dst = cv2.warpPerspective(pts_src, h, (pts_src.shape[1],pts_src.shape[0]))
+#im_dst = cv2.warpPerspective(pts_src, h, (pts_src.shape[1],pts_src.shape[0]))
 # print(im_dst)
 ###################################################
 
-pts_src=np.array([[-18.5],[101.5],[1]])  # planar object points 
+pts_src=np.array([[-138.5],[138.5],[1]])  # planar object points 
 h=np.array([[2.59275014,1.22742257,703.045769],[0.0176127956,-0.801686794,417.718048],[0.000208861672,0.00224224441,1.00000000]])
 pts_dst=np.zeros((3,1)) # image points
 #finding homography
 import cv2
+from numpy.linalg import inv
+#h=inv(h)
 pts_dst=np.matmul(h,pts_src)
+pts_dst[0]=pts_dst[0]/pts_dst[2]
+pts_dst[1]=pts_dst[1]/pts_dst[2]
+pts_dst[2]=1
 print(pts_dst)
 # #,[-138.5,-78.5],[-101.5,-78.5],[-101.5,-41.5],[-138.5,-41.5]             ,[-41.5,341.5,0],[-41.5,378.5,0],[-78.5,378.5,0
